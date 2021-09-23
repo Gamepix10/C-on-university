06290eb6f8cb06820c6ba695713f352c76afdb46
Gamepix10 <ianulov2003@mail.ru>
2021-09-23 21:03:49 +0500

    lab3_1

--- lab3.c
+++ lab3.c
@@ -7,21 +7,26 @@ int main()
   scanf("%lf", &h );
   printf("Vvedite nachalnoe znachenie x:");
   scanf("%lf",&x);
-  printf(" x                f(x) ");
-  printf(" --------------------- ");
-  do {
-    printf(" \n %lf ", x);
-    y = pow(x, 2) * sin( sqrt(pow (x, 1.0 / 3)) - 3);
-    printf(" \n %lf ", y);
-    x=+k;
-  }while(x >= -2 && x <= 0)
+  printf(" x \t f(x) ");
+  printf(" \n--------------------- ");
 
-  do{
-    printf(" \n %lf ", x);
-    y = sqrt(x) * cos(2*x);
-    printf(" \n %lf ", y);
-    x=+k;
-  }while( x > 0 && x <= 1 );
+  
+    do {
+      printf(" \n %lf ", x);
+      y = pow(x, 2) * sin( sqrt(pow (x, 1.0 / 3)) - 3);
+      printf(" \t %lf ", y);
+      x = x + h;
+    }while(x >= -2.0 && x <= 0.0);
+
+
+
+    do{
+      printf(" \n %lf ", x);
+      y = sqrt(x) * cos(2*x);
+      printf(" \t %lf ", y);
+      x = x + h;
+    }while( x > 0.0 && x <= 1.0 );
+
 
   return 0;
 }
